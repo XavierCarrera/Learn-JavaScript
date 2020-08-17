57ddc6086d2425e03392b67221907ab55b9b2014
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-17 08:13:22 -0500

    Simon starts

--- asincronismo.js
+++ asincronismo.js
@@ -1,6 +1,10 @@
 const API_URL = 'https://swapi.co/api/'
 	const PEOPLE_URL = 'people/:id'
 
-	const URL = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+    const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+    const opts = { crossDomain: true}
 
-	$.get(URL, {crossDomain: true}, function(){ }) 
+	$.get(URL, {crossDomain: true}, function(){ 
+        
+
+    }) 
