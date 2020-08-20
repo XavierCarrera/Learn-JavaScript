acb53cbc4459bfa1beda52aef6464201b2c7fc8f
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-20 12:42:03 -0500

    Order callback

--- asincronismo.js
+++ asincronismo.js
@@ -16,15 +16,32 @@ const opts = { crossDomain: true }
 // const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}` 
 // const opts = { crossDomain: true }
 
-const onPeopleResponse = function (persona) {
-    console.log(`Hola, you soy ${persona.name}`)
-}
+// const onPeopleResponse = function (persona) {
+//     console.log(`Hola, yo soy ${persona.name}`)
+// }
 
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', 1)}` 
-    $.get(url, opts, onPeopleResponse)
+    $.get(url, opts, function (persona) {
+        console.log(`Hola, yo soy ${persona.name}`)
+
+        if (callback) {
+            callback()
+        }
+    })
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+obtenerPersonaje(1, function () {
+    obtenerPersonaje(2, function () {
+        obtenerPersonaje(3, function () {
+            obtenerPersonaje(4, function () {
+                obtenerPersonaje(5, function() {
+                    obtenerPersonaje(6, function () {
+                        obtenerPersonaje(7,)
+                    })
+                })
+
+            })
+        })
+    })
+})
