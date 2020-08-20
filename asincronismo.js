ef97ab259d7686091632d246aa1d250798434cd2
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-20 13:38:10 -0500

    First promises

--- asincronismo.js
+++ asincronismo.js
@@ -13,35 +13,52 @@ const API_URL = "https://swapi.dev/api/"
 const PEOPLE_URL = "people/:id"
 const opts = { crossDomain: true }
 
-// const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}` 
-// const opts = { crossDomain: true }
 
-// const onPeopleResponse = function (persona) {
-//     console.log(`Hola, yo soy ${persona.name}`)
-// }
-
-function obtenerPersonaje(id, callback) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', 1)}` 
-    $.get(url, opts, function (persona) {
-        console.log(`Hola, yo soy ${persona.name}`)
-
-        if (callback) {
-            callback()
-        }
+function obtenerPersonaje(id) {
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}` 
+        $
+            .get(url, opts, function (data) {
+                resolve(data)
+            })
+            .fail(() => reject(id))
     })
 }
 
-obtenerPersonaje(1, function () {
-    obtenerPersonaje(2, function () {
-        obtenerPersonaje(3, function () {
-            obtenerPersonaje(4, function () {
-                obtenerPersonaje(5, function() {
-                    obtenerPersonaje(6, function () {
-                        obtenerPersonaje(7,)
-                    })
-                })
+function onError (id) {
+    console.log(`Sucedió un error al obtener el personaje ${id}`)
+}
 
-            })
-        })
-    })
-})
+var id = [1, 2, 3, 4, 5, 6]
+var promesas = id.map(id => obtenerPersonaje(id))
+Promise
+    .all(promesas)
+    .then(personajes => console.log(personajes))
+    .catch(onError)
+
+// obtenerPersonaje(1)
+//     .then(personaje1 => {
+//         console.log(`El personaje 1 es ${personaje1.name}`)
+//         return obtenerPersonaje(2)
+//     })
+//     .then(personaje2 => {
+//         console.log(`El personaje 2 es ${personaje2.name}`)
+//         return obtenerPersonaje(3)
+//     })
+//     .then(personaje3 => {
+//         console.log(`El personaje 3 es ${personaje3.name}`)
+//         return obtenerPersonaje(4)
+//     })
+//     .then(personaje4 => {
+//         console.log(`El personaje 4 es ${personaje4.name}`)
+//         return obtenerPersonaje(5)
+//     })
+//     .then(personaje5 => {
+//         console.log(`El personaje 5 es ${personaje5.name}`)
+//         return obtenerPersonaje(6)
+//     })
+//     .then(personaje6 => {
+//         console.log(`El personaje 6 es ${personaje6.name}`)
+//     })
+//     .catch(onError)
+
