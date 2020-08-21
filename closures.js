62145b6807c72b2fc45aab8d0cc462ccce631381
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-21 17:54:45 -0500

    Closures done

--- closures.js
+++ closures.js
@@ -1,3 +1,5 @@
+// CLOSURE
+
 const moneyBox = () => {
     var saveCoins = 0;
     const countCoins = () => {
@@ -12,3 +14,51 @@ let myMoneyBox = moneyBox();
 myMoneyBox(4);
 myMoneyBox(6);
 myMoneyBox(10);
+
+// LEXICAL
+
+const buildCount = (i) => {
+    let count = i;
+    const displayCount = () => {
+        console.log(count++)
+    };
+    return displayCount;
+};
+
+const myCount = buildCount(1);
+myCount();
+myCount();
+myCount();
+
+const myOtherCount = buildCount(10);
+myOtherCount();
+myOtherCount();
+
+// PRIVATE VARIABLES
+
+const person = () => {
+    var saveName = "Name";
+    return {
+        getName: () => {
+            return saveName;
+        },
+        setName: (name) => {
+            saveName = name;
+        }
+    }
+}
+
+newPerson = person ();
+console.log(newPerson.getName());
+newPerson.setName("Xavier");
+console.log(newPerson.getName());
+
+// LOOPS
+
+const loopFunctions = () => {
+    for (let i = 0; i <10; i++) {
+        setTimeout(() => {
+            console.log(i);
+        }, 1000)
+    }
+}
